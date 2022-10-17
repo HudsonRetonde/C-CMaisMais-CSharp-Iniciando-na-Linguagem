b68ca28a8beaa71957e76b220ba61cca60c2473b
Hudson Retonde <hudsonsretonde@gmail.com>
2022-10-17 20:33:27 -0300

    Terminando os laços de repetição.

---
+++ C e C++ Udemy/Numeros Pares de 10 a 20.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+void main(){
+
+    setlocale(LC_ALL,"");
+
+    int valor1, i;
+
+    for(i = 10; i<= 20; i++){
+        if(i % 2 ==0){
+            printf("%d é par \n", i);
+        } else {
+            printf("%d é ímpar \n", i);
+        }
+    };
+}
