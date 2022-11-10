ce81e1aa540c23c70364b0c6e37aef6f33baf094
Hudson Retonde <hudsonsretonde@gmail.com>
2022-11-09 21:56:38 -0300

    Exercício faculdade.

---
+++ C e C++ Udemy/contador simples.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+main() {
+
+    int p, q;
+    p = 1; q = 2;
+    printf("valor de p = %d\n", p);
+    printf("valor de q = %d\n", q);
+    printf("\n Criando o Incremento: p++\n");
+    p++;
+    printf("\n Criando o Decremento: q--\n");
+    q--;
+    printf("\n O novo valor para p = %d\n",p);
+    printf("\n O novo valor para q = %d\n",q);
+
+
+    return(0);
+}
