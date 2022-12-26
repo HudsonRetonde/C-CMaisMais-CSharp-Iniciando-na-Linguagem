bcb43272cf27aee52c0d576d835dee4a91b7c90e
Hudson Retonde <hudsonsretonde@gmail.com>
2022-12-26 16:04:29 -0300

    O Primeiro Objeto.

---
+++ C#/C# Alura/Orientação a Objetos CSharp/ByteBank/ByteBank/Program.cs
@@ -0,0 +1,9 @@
+﻿using ByteBank;
+
+ContaCorrente contaDoAndre = new ContaCorrente();
+contaDoAndre.titular = "André Silva";
+contaDoAndre.numero_agencia = 15;
+contaDoAndre.conta = "1010-x";
+contaDoAndre.saldo = 100;
+
+Console.WriteLine("O saldo da conta do André é " + contaDoAndre.saldo);
