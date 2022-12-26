306abaf44ce607592e1f453943b92881b72affb3
Hudson Retonde <hudsonsretonde@gmail.com>
2022-12-26 16:31:12 -0300

    O Primeiro Método.

--- C#/C# Alura/Orientação a Objetos CSharp/ByteBank/ByteBank/Program.cs
+++ C#/C# Alura/Orientação a Objetos CSharp/ByteBank/ByteBank/Program.cs
@@ -6,4 +6,8 @@ contaDoAndre.numero_agencia = 15;
 contaDoAndre.conta = "1010-x";
 contaDoAndre.saldo = 100;
 
-Console.WriteLine("O saldo da conta do André é " + contaDoAndre.saldo);
+Console.WriteLine("O saldo da conta do André é " + contaDoAndre.saldo +" reais.");
+
+contaDoAndre.Depositar(100);
+
+Console.WriteLine("O saldo da conta do André pós-depósito é " + contaDoAndre.saldo + " reais.");
