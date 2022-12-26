6db5318163cbefeaf1cb0fd472c720cc1a9c9f22
Hudson Retonde <hudsonsretonde@gmail.com>
2022-12-26 16:54:30 -0300

    Realizando um saque.

--- C#/C# Alura/Orientação a Objetos CSharp/ByteBank/ByteBank/ContaCorrente.cs
+++ C#/C# Alura/Orientação a Objetos CSharp/ByteBank/ByteBank/ContaCorrente.cs
@@ -17,5 +17,17 @@ namespace ByteBank
         {
             this.saldo += valor;
         }
+
+        public bool Sacar(double valor)
+        {
+            if(valor <= this.saldo)
+            {
+                this.saldo -= valor;
+                return true;
+            } else
+            {                
+                return false;
+            }
+        }
     }   
 }
--- C#/C# Alura/Orientação a Objetos CSharp/ByteBank/ByteBank/Program.cs
+++ C#/C# Alura/Orientação a Objetos CSharp/ByteBank/ByteBank/Program.cs
@@ -11,3 +11,12 @@ Console.WriteLine("O saldo da conta do André é " + contaDoAndre.saldo +" reais
 contaDoAndre.Depositar(100);
 
 Console.WriteLine("O saldo da conta do André pós-depósito é " + contaDoAndre.saldo + " reais.");
+
+if (contaDoAndre.Sacar(50) == true)
+{  
+    Console.WriteLine("O saldo da conta do André pós-saque é " + contaDoAndre.saldo + " reais.");
+} else {
+        Console.WriteLine("O valor solicitado é maior que o saldo disponíel em conta. Operação invalidada...");
+}
+
+
