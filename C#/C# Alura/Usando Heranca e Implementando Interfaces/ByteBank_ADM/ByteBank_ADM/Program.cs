3b17a1a8582dbaaa89719dbb04e30088c95874dc
Hudson Retonde <hudsonsretonde@gmail.com>
2022-12-29 11:19:11 -0300

    Classe Funcionários.

---
+++ C#/C# Alura/Usando Heranca e Implementando Interfaces/ByteBank_ADM/ByteBank_ADM/Program.cs
@@ -0,0 +1,9 @@
+﻿using ByteBank_ADM.Funcionarios;
+
+Funcionario pedro = new Funcionario();
+pedro.Nome = "Pedro Malazrtes";
+pedro.Cpf = "45698712166";
+pedro.Salario = 2000;
+
+Console.WriteLine(pedro.Nome);
+Console.WriteLine(pedro.GetBonificacao());
