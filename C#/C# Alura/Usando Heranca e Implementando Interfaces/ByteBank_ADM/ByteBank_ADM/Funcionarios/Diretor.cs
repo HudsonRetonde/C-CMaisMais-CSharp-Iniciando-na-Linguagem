8290e6dbf0c95d86d0fa6cd3c2c8fba36873f1fb
Hudson Retonde <hudsonsretonde@gmail.com>
2022-12-29 16:26:38 -0300

    Sobrecarga de método.

--- C#/C# Alura/Usando Heranca e Implementando Interfaces/ByteBank_ADM/ByteBank_ADM/Funcionarios/Diretor.cs
+++ C#/C# Alura/Usando Heranca e Implementando Interfaces/ByteBank_ADM/ByteBank_ADM/Funcionarios/Diretor.cs
@@ -6,16 +6,15 @@ using System.Threading.Tasks;
 
 namespace ByteBank_ADM.Funcionarios
 {
-    public class Funcionario
+    public class Diretor
     {
         public string Nome { get; set; }
         public string Cpf { get; set; }
         public double Salario { get; set; }
         public double GetBonificacao()
         {
-            
-            return this.Salario * 0.10;
+
+            return this.Salario;
         }
-          
     }
 }
--- C#/C# Alura/Usando Heranca e Implementando Interfaces/ByteBank_ADM/ByteBank_ADM/Program.cs
+++ C#/C# Alura/Usando Heranca e Implementando Interfaces/ByteBank_ADM/ByteBank_ADM/Program.cs
@@ -1,4 +1,5 @@
 ﻿using ByteBank_ADM.Funcionarios;
+using ByteBank_ADM.Utilitario;
 
 Funcionario pedro = new Funcionario();
 pedro.Nome = "Pedro Malazrtes";
@@ -7,3 +8,21 @@ pedro.Salario = 2000;
 
 Console.WriteLine(pedro.Nome);
 Console.WriteLine(pedro.GetBonificacao());
+
+Console.WriteLine();
+
+Diretor roberta = new Diretor();
+roberta.Nome = "Roberta Souza";
+roberta.Cpf = "98745622145";
+roberta.Salario = 5000;
+
+Console.WriteLine(roberta.Nome);
+Console.WriteLine(roberta.GetBonificacao());
+
+Console.WriteLine();
+
+GerenciadorDeBonificacao gerenciador = new GerenciadorDeBonificacao();
+gerenciador.Registrar(pedro);
+gerenciador.Registrar(roberta);
+
+Console.WriteLine("Total de bonificações: " + gerenciador.TotalDeBonificacao);
