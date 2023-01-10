bd145c938a872d336d599caad2e86c13317667b0
Hudson Retonde <hudsonsretonde@gmail.com>
2023-01-10 16:04:54 -0300

    Salario Imposto.

---
+++ C#/C# Nelio/CursoProgramacaoCSharp/FuncionarioImposto/FuncionarioImposto/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Globalization;
+
+namespace FuncionarioImposto
+   
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario func = new Funcionario();   
+
+            Console.WriteLine("Nome: ");
+            func.Nome = Console.ReadLine();
+
+            Console.WriteLine("Salario Bruto: ");
+            func.SalarioBruto = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Imposto: ");
+            func.Imposto = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("");
+            Console.WriteLine("Funcionário: " + func);
+
+            Console.WriteLine("");
+            Console.WriteLine("Deseja aumentar o salário em qual porcentagem? ");
+            double porcent = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            func.AumentarSalario(porcent);
+
+            Console.WriteLine("");
+            Console.WriteLine("Dados atualizados: " + func);
+
+        }
+    }
+}
