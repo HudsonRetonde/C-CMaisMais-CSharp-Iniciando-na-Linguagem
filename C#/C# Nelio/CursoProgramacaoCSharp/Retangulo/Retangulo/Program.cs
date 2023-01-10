7645093f57407e21a3bf0eff506d9bf07888246e
Hudson Retonde <hudsonsretonde@gmail.com>
2023-01-10 15:47:19 -0300

    Area, Perimetro e Diagonal do Retangulo.

---
+++ C#/C# Nelio/CursoProgramacaoCSharp/Retangulo/Retangulo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Globalization;
+
+namespace Retangulo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Lados ret = new Lados();
+
+            Console.WriteLine("Entre com a largura do retângulo: ");
+            ret.Largura = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("");
+            Console.WriteLine("Entre com a altura do retângulo: ");
+            ret.Altura = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Area: " + ret.Area().ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Perímetro: " + ret.Perimetro().ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Diagonal: " + ret.Diagonal().ToString("F2", CultureInfo.InvariantCulture));
+
+        }
+    }
+}
