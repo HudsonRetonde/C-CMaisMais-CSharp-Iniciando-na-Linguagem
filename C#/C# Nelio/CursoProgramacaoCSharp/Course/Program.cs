7a8d10e6a58741aac440c4f44ad7235164074b1d
Hudson Retonde <hudsonsretonde@gmail.com>
2023-01-15 16:46:58 -0300

    Construtores e Listas.

---
+++ C#/C# Nelio/CursoProgramacaoCSharp/Course/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Globalization;
+using System.Net.WebSockets;
+using ContaBancaria;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaBancaria conta;
+
+            Console.Write("Entre com o número da conta: ");
+            int numero = int.Parse(Console.ReadLine());
+            Console.Write("Entre com o tiular da conta: ");
+            string titular = Console.ReadLine();
+            Console.Write("Haverá depósito inidicial (s/n) ? ");
+            char resp = char.Parse(Console.ReadLine());
+            if (resp == 's' || resp == 'S')
+            {
+                Console.Write("Entre com o valor do depósito inicial: ");
+                double depositoInicial = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                conta = new ContaBancaria(numero, titular, depositoInicial);
+            }
+            else
+            {
+                conta = new ContaBancaria (numero, titular);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Dados da conta: ");
+            Console.WriteLine(conta);
+
+            Console.WriteLine();
+            Console.WriteLine("Entre um valor para depósito: ");
+            double deposito = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture)
+            conta.Deposito(quantia);
+            Console.WriteLine();
+            Console.WriteLine(conta);
+
+            Console.WriteLine();
+            Console.WriteLine("Entre um valor para saque: ");
+            double saque = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture)
+            conta.Saque(quantia);
+            Console.WriteLine();
+            Console.WriteLine(conta);
+
+        }
+
+    }
+}
---
+++ C#/C# Nelio/CursoProgramacaoCSharp/TrabalhandoComListas/Program.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+
+namespace TrabalhandoComListas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> list = new List<string>();
+
+            list.Add("Maria");
+            list.Add("José");
+            list.Add("Thiago");
+            list.Add("João");
+            list.Insert(2, "Marcos");
+
+            foreach (string obj in list) {
+                Console.WriteLine(obj);
+            }
+
+            Console.WriteLine("O tamanho da lista é " + list.Count);
+
+            string s1 = list.Find(x => x[0] == 'T');
+            Console.WriteLine("A primeira ocorrência de um nome com a letra T é: " + s1);
+
+            string s2 = list.FindLast(x => x[0] == 'M');
+            Console.WriteLine("A última ocorrência de um nome com a letra M é: " + s2);
+
+            int pos1 = list.FindIndex(x => x[0] == 'J');
+            Console.WriteLine("A primeira posição da letra J é: " + pos1);
+
+            int pos2 = list.FindLastIndex(x => x[0] == 'J');
+            Console.WriteLine("A última posição da letra J é: " + pos2);
+
+            List<string> list2 = list.FindAll(x => x.Length >= 5);
+            Console.WriteLine("---------------------");
+            foreach (string obj in list2)            
+            {
+                Console.WriteLine(obj);
+            }
+
+            list.Remove("Thiago");
+            Console.WriteLine("---------------------");
+            foreach (string obj in list)
+            {
+                Console.WriteLine(obj);
+            }
+
+            list.RemoveAll(x => x[0] == 'M');
+            Console.WriteLine("---------------------");
+            foreach (string obj in list)
+            {
+                Console.WriteLine(obj);
+            }
+        }
+    }
+}
