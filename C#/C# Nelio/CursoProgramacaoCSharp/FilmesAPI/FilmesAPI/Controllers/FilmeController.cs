5b6b315bf9269039efecc04d6d1c94e1fc251c6f
Hudson Retonde <hudsonsretonde@gmail.com>
2023-01-23 17:51:34 -0300

    Paginando resultados

--- C#/C# Nelio/CursoProgramacaoCSharp/FilmesAPI/FilmesAPI/Controllers/FilmeController.cs
+++ C#/C# Nelio/CursoProgramacaoCSharp/FilmesAPI/FilmesAPI/Controllers/FilmeController.cs
@@ -8,17 +8,26 @@ namespace FilmesAPI.Controllers;
 public class FilmeController : ControllerBase
 {
     private static List<Filme> filmes = new List<Filme>();
+    private static int id = 0;
+
     [HttpPost]
     public void AdcionaFilme([FromBody] Filme filme)
     {
+        filme.Id = id++;
         filmes.Add(filme);
         Console.WriteLine(filme.Titulo);
         Console.WriteLine(filme.Duracao);
     }
 
     [HttpGet]
-    public List<Filme> RecuperaFilmes() 
+    public IEnumerable<Filme> RecuperaFilmes([FromQuery] int skip = 0, [FromQuery] int take = 10) 
     {
-        return filmes;
+        return filmes.Skip(skip).Take(take);
+    }
+
+    [HttpGet("{id}")]
+    public Filme? RecuperaFilmePorId(int id) 
+    {
+        return filmes.FirstOrDefault(filme => filme.Id == id);
     }
 }
