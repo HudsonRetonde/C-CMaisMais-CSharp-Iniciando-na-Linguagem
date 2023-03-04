92722c1f49f9f736acfb262271341aa2ebf46fa3
Hudson Retonde <hudsonsretonde@gmail.com>
2023-03-04 13:07:47 -0300

    Criando as classes de cinema

--- C#/C# Nelio/CorseLogicaAula1/CorseLogicaAula1/Program.cs
+++ C#/C# Nelio/CorseLogicaAula1/CorseLogicaAula1/Program.cs
@@ -2,13 +2,29 @@
 
 namespace CourseLogicaAula1
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            SByte x = 100;
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Entre com a string");
+			string input = Console.ReadLine();
 
-            Console.WriteLine(x);
-        }
-    }
+			Console.WriteLine("Entre com uma letra");
+			char serachInput = Console.ReadLine()[0];
+
+			int searchIndex = input.IndexOf(serachInput);
+			Console.WriteLine("O index do caracter {0} na string é {1}", serachInput, searchIndex);
+
+
+			Console.WriteLine("entre com o primeiro nome: ");
+			string firstName =  Console.ReadLine();
+			Console.WriteLine("Entre com o sobrenome: ");
+			string lastName = Console.ReadLine();
+
+			string fullName = string.Concat(firstName, " ", lastName);
+			Console.WriteLine("Seu nome completo é {0}", fullName);
+
+			Console.ReadKey();
+		}
+	}
 }
