775215d5862a26a111d1db5fe322c7807346132a
Hudson Retonde <hudsonsretonde@gmail.com>
2023-09-07 12:26:45 -0300

    logando sql

--- Alura.Loja/Alura.Loja.Testes.ConsoleApp/Program.cs
+++ Alura.Loja/Alura.Loja.Testes.ConsoleApp/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +15,10 @@ namespace Alura.Loja.Testes.ConsoleApp
         {
             using (var contexto = new LojaContext())
             {
+                var serviceProvider = contexto.GetInfrastructure<IServiceProvider>();
+                var loggerFactory = serviceProvider.GetService<ILoggerFactory>();
+                loggerFactory.AddProvider(SqlLoggerProvider.Create());
+
                 var produtos = contexto.Produtos.ToList();
                 foreach (var produto in produtos)
                 {
@@ -27,9 +34,7 @@ namespace Alura.Loja.Testes.ConsoleApp
 
 				var p1 = produtos.Last();
                 p1.Nome = "007 - Um novo Dia para Morrer"
-;               contexto.SaveChanges();
-
-				
+;               contexto.SaveChanges();				
 
 				Console.WriteLine("###################");
 				produtos = contexto.Produtos.ToList();
